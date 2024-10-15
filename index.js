075bb78803442110abc3699a26ca2870a55f6681
Shubhrav139 <shubhrav139@gmail.com>
2024-10-16 02:12:24 +0530

    add: function should return the sum of numbers when input contains two digits comma separated

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function add(num) {
-  if (num) {
+  if (num.length > 1) {
+    let [val1, val2] = num.split(",");
+    return parseInt(val1) + parseInt(val2);
+  } 
+  else if (num.length == 1) {
     return parseInt(num);
-  } else {
+  }
+  else {
     return 0;
   }
 }
--- index.test.js
+++ index.test.js
@@ -8,4 +8,8 @@ describe("basic test case", () => {
   test("should return the number itself when input is single digit number", () => {
     expect(add("1")).toBe(1);
   });
+
+  test("should return the sum of numbers when input contains two digits comma separated", () => {
+    expect(add("1,2")).toBe(3);
+  });
 });
