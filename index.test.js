0458a68dae62cf773cc6b610e0bd519612ef4f1f
Shubhrav139 <shubhrav139@gmail.com>
2024-10-16 02:02:09 +0530

    add: function should return the number itself when input is single digit number

--- index.test.js
+++ index.test.js
@@ -4,4 +4,8 @@ describe("basic test case", () => {
   test("should return 0 when input is an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  test("should return the number itself when input is single digit number", () => {
+    expect(add("1")).toBe(1);
+  });
 });
